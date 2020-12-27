d074dba7a1cceea78eaeeeaf646e2bd32889ecc9
Zhubaiali <69970253+Zhubaiali@users.noreply.github.com>
2020-12-27 16:47:06 +0800

    模块化

--- c2048/board.h
+++ c2048/board.h
@@ -1,1 +1,14 @@
 
+#indef BOARD_H_INCLUDE
+#define BOARD_H_INCLUDE
+
+#include<stdio.h>
+extern void StartGame();
+extern void InsertANumber();
+extern void  Output();
+extern int MoveLeft();
+extern int MoveRight();
+extern int MoveUp();
+extern int MoveDown();
+
+#endif//BOARD_H_INCLUDE
